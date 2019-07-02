92faf8ca4418d4783d7de6eec322482486034ba5
Tristian Azuara <tristiana@4thdown.co>
2019-07-01 17:56:30 -0700

    Fix source issue (types)

--- sub-package/sum.c
+++ sub-package/sum.c
@@ -1,4 +1,4 @@
 
 int sum(int a, int b) {
-	return a + b
+	return a + b;
 }
--- sub-package/sum.go
+++ sub-package/sum.go
@@ -1,12 +1,10 @@
 package subpkg
 
 /*
-#cgo
-
 int sum(int, int);
 */
 import "C"
 
 func Sum(a, b int) int {
-	return C.sum(C.int(a), C.int(b))
+	return int(C.sum(C.int(a), C.int(b)))
 }
